c8d82e99902fc3f7f8d3d65026f9ac1ef77292e1
Rohit Vipin Mathews <rohitvipin@gmail.com>
2017-04-21 15:08:08 +0530

    test

--- RohitAzureOfflineSyncService/Controllers/TodoItemController.cs
+++ RohitAzureOfflineSyncService/Controllers/TodoItemController.cs
@@ -19,10 +19,7 @@ namespace RohitAzureOfflineSyncService.Controllers
         }
 
         // GET tables/TodoItem
-        public IQueryable<TodoItem> GetAllTodoItems()
-        {
-            return Query();
-        }
+        public IQueryable<TodoItem> GetAllTodoItems() => Query();
 
         // GET tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
         public SingleResult<TodoItem> GetTodoItem(string id)
